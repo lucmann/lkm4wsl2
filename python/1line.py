e6855b23a567a975b3ce3afc110d5c9d8da37e2c
Ma Chao <onion_0709@yahoo.com>
2016-10-29 19:18:38 +0800

    my heart will go on

--- python/1line.py
+++ python/1line.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
 
+import os
 import sys
+import time
 
-#print '\n'.join([''.join([(sys.argv[1][(x-y)%len(sys.argv[1])] if ((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3 <= 0 else ' ') for x in range(-30,30)]) for y in range(15,-15,-1)])
+scale = [(0.07, 0.14), (0.05, 0.1)]
 
-# The smaller factors of x,y are, the bigger the heart is
-print '\n'.join([''.join([('LoveLn'[(x-y)%6] if ((x*0.025)**2+(y*0.05)**2-1)**3-(x*0.025)**2*(y*0.05)**3 <= 0 else ' ') for x in range(-30,30)]) for y in range(15,-15,-1)])
+def my_heart():
+	for i in range(len(scale)):
+		os.system('clear')
+		print('\n'.join([''.join([('LoveLn'[(x-y)%6] if ((x*scale[i][0])**2+(y*scale[i][1])**2-1)**3-(x*scale[i][0])**2*(y*scale[i][1])**3 <= 0 else ' ') for x in range(-30,30)]) for y in range(15,-15,-1)]))
+		time.sleep(0.2)
+
+if __name__ == '__main__':
+	print('\033[1;31m')
+	while (True):
+		my_heart()
+		time.sleep(0.3)
