3ff84b161e8c989748856190cd19ed415cc12825
luc <lucmann@qq.com>
2021-06-09 20:14:25 +0800

    style: using vim-clang-format
    Signed-off-by: luc <lucmann@qq.com>

--- hello.c
+++ hello.c
@@ -8,16 +8,18 @@ MODULE_DESCRIPTION("A Hello World Kernel Module for WSL2.");
 MODULE_VERSION("0.1.0");
 
 static char *name = "world";
-module_param(name,  charp, S_IRUGO);
+module_param(name, charp, S_IRUGO);
 MODULE_PARM_DESC(name, "The name to display in /var/log/kern.log");
 
-static int __init hello_wsl2_init(void) {
-    printk(KERN_INFO "WSL2: Hello %s from the WSL2 LKM!\n", name);
-    return 0;
+static int __init hello_wsl2_init(void)
+{
+	printk(KERN_INFO "WSL2: Hello %s from the WSL2 LKM!\n", name);
+	return 0;
 }
 
-static void __exit hello_wsl2_exit(void) {
-    printk(KERN_INFO "WSL2: Goodbye %s from the WSL2 LKM!\n", name);
+static void __exit hello_wsl2_exit(void)
+{
+	printk(KERN_INFO "WSL2: Goodbye %s from the WSL2 LKM!\n", name);
 }
 
 module_init(hello_wsl2_init);
